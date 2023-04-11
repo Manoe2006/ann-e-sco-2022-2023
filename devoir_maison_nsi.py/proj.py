23d289e22e0d72a45c2e2fb4768180a103af5454
Manoe2006 <ioanattebe@gmail.com>
2023-04-11 19:09:49 +0000

    Momo help

--- devoir_maison_nsi.py/proj.py
+++ devoir_maison_nsi.py/proj.py
@@ -1,15 +1,14 @@
+# Import des bibliothèques nécessaires
 from itertools import combinations
 from math import sqrt
-import tkinter as tk
-from tkinter import ttk
 import matplotlib.pyplot as plt
 
-
+# Fonction pour calculer la distance entre deux points
 def distance(pt1, pt2):
     """Calcule la distance entre deux points."""
     return sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
 
-
+# Fonction pour vérifier si quatre points donnés forment un carré
 def est_carre(points):
     """Vérifie si les quatre points donnés forment un carré."""
     if len(set(tuple(pt) for pt in points)) != 4:
@@ -27,7 +26,7 @@ def est_carre(points):
     else:
         return False
 
-
+# Fonction pour vérifier si quatre points donnés forment un rectangle
 def est_rectangle(points):
     """Vérifie si les quatre points donnés forment un rectangle."""
 
@@ -47,7 +46,7 @@ def est_rectangle(points):
     else:
         return False
 
-
+# Fonction pour vérifier si trois points donnés forment un triangle rectangle
 def est_triangle_rectangle(points):
     """Vérifie si les trois points donnés forment un triangle rectangle."""
 
@@ -65,6 +64,7 @@ def est_triangle_rectangle(points):
     else:
         return False
 
+# Fonction pour afficher le nuage de points à partir d'un fichier CSV
 def nuagedepts():
     Y=[]
     f=open("C:\\Users\\ioana\\Desktop\\nsi\\ann-e-sco-2022-2023\\devoir_maison_nsi.py\\mano_test.csv","r")
@@ -83,6 +83,7 @@ def nuagedepts():
     plt.show()
     print(H)
 
+# Fonction pour vérifier si un ensemble de points forme un polygone donné (carré, rectangle ou triangle rectangle)
 def polygone(shape_function):
     Z = []
     fil=open("C:\\Users\\ioana\\Desktop\\nsi\\ann-e-sco-2022-2023\\devoir_maison_nsi.py\\mano_test.csv","r")
@@ -116,90 +117,24 @@ def polygone(shape_function):
     plt.show()
 
 
-
-
-from tkinter import filedialog
-
-# Les fonctions distance, est_carre, est_rectangle, est_triangle_rectangle, nuagedepts et polygone restent les mêmes
-
-# '''def browse_file():
-#     file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
-#     if file_path:
-#         file_path_label.config(text=file_path)
-
-# def analyze_points():
-#     file_path = file_path_label.cget("text")
-#     if file_path:
-#         shape = shape_var.get()
-#         if shape == "Nuage de points":
-#             nuagedepts()
-#         elif shape == "Carré":
-#             polygone(est_carre)
-#         elif shape == "Rectangle":
-#             polygone(est_rectangle)
-#         elif shape == "Triangle rectangle":
-#             polygone(est_triangle_rectangle)
-#     else:
-#         result_label.config(text="Veuillez sélectionner un fichier CSV.")
-
-# def main():
-#     global file_path_label, result_label, shape_var
-
-#     window = tk.Tk()
-#     window.title("Analyse de nuages de points")
-
-#     style = ttk.Style()
-#     style.configure("TLabel", font=("Arial", 14))
-#     style.configure("TButton", font=("Arial", 14))
-
-#     title_label = ttk.Label(window, text="Analyse de nuages de points", font=("Arial", 16))
-#     title_label.pack(pady=10)
-
-#     browse_button = ttk.Button(window, text="Parcourir", command=browse_file)
-#     browse_button.pack(pady=10)
-
-#     file_path_label = ttk.Label(window, text="", wraplength=400)
-#     file_path_label.pack(pady=10)
-
-#     shape_label = ttk.Label(window, text="Sélectionnez la forme à rechercher:")
-#     shape_label.pack(pady=10)
-
-#     shape_var = tk.StringVar()
-#     shape_var.set("Nuage de points")
-#     shape_options = ["Nuage de points", "Carré", "Rectangle", "Triangle rectangle"]
-
-#     shape_menu = ttk.OptionMenu(window, shape_var, *shape_options)
-#     shape_menu.pack(pady=10)
-
-#     analyze_button = ttk.Button(window, text="Analyser les points", command=analyze_points)
-#     analyze_button.pack(pady=10)
-
-#     result_label = ttk.Label(window, text="", wraplength=400)
-#     result_label.pack(pady=10)
-
-#     quit_button = ttk.Button(window, text="Quitter", command=window.quit)
-#     quit_button.pack(pady=10)
-
-#     window.mainloop()
-
-# if __name__ == "__main__":
-#     main()'''
-
+# Import des bibliothèques pour l'interface graphique
+import tkinter
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from ttkthemes import ThemedTk
-import matplotlib.pyplot as plt
-from itertools import combinations
-from math import sqrt
+from tkinter import PhotoImage
 
-# Les fonctions distance, est_carre, est_rectangle, est_triangle_rectangle, nuagedepts et polygone restent les mêmes
 
+
+
+# Fonction pour ouvrir un explorateur de fichiers et sélectionner un fichier CSV
 def browse_file():
     file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
     if file_path:
         file_path_label.config(text=file_path)
 
+# Fonction pour analyser les points en fonction de la forme sélectionnée
 def analyze_points():
     file_path = file_path_label.cget("text")
     if file_path:
@@ -215,12 +150,15 @@ def analyze_points():
     else:
         result_label.config(text="Veuillez sélectionner un fichier CSV.")
 
+# Fonction principale pour créer l'interface graphique et gérer les formes
 def main():
     global file_path_label, result_label, shape_var
 
+    # Création de la fenêtre principale avec un thème spécifique
     window = ThemedTk(theme="arc")  # Vous pouvez changer le thème ici
     window.title("Analyse de nuages de points")
 
+    # Création des widgets (labels, boutons, etc.) et placement dans la fenêtre
     style = ttk.Style()
     style.configure("TLabel", font=("Arial", 14))
     style.configure("TButton", font=("Arial", 14))
@@ -253,6 +191,7 @@ def main():
     quit_button = ttk.Button(window, text="Quitter", command=window.quit)
     quit_button.pack(pady=10)
 
+    #Démarrage de la boucle d'événements de la fenêtre principale
     window.mainloop()
 
 if __name__ == "__main__":
---
+++ exoecole20/09/v.py
@@ -0,0 +1,268 @@
+from math import sqrt
+from itertools import *
+import matplotlib.pyplot as plt
+"Ce proramme permet de reconnaitre certaines figures particulieres contenues dans un NUAGE DE POINT "
+#programme principal avec le fichier et les minis focntions y compris
+def princip(figure):
+    V=[]
+    f=open("C:\\Users\\LENOVO\\Documents\\tester.csv","r")
+    lignes=f.readlines()
+    P=[]
+    B=[]
+    M=[]
+    for i in range(1,len(lignes)):
+        li=lignes[i]
+        V=li.split(";")
+        P.append([V[0],V[1]])
+    for i in range(len(P)):
+        s=float(P[i][0])
+        k=float(P[i][1])
+        B.append([s,k])
+    x=[]
+    y=[]
+    for i in range(len(B)):
+        x.append(B[i][0])
+        y.append(B[i][1])
+    #affichage des points en rouge avec plt.scatter
+    plt.scatter(x,y,c="red")
+    plt.show()
+    #choix des figures }}
+    if (figure==carré):
+        carré(B)
+    if (figure==TriangleR):
+        TriangleR(B)
+    if (figure==Rectangle):
+        Rectangle(B)
+    if  (figure==Triangle_R_Iso):
+        Triangle_R_Iso(B)
+    if (figure==Triangle_Equi):
+        Triangle_Equi(B)
+    if (figure==Parallelogramme):
+        Parallelogramme(B)
+    if (figure==n_figure):
+        n_figure(B)
+#les minis fonctions sont rappellé ici
+#n designe toutes les figures
+def n_figure(B):
+    carré(B)
+    Rectangle(B)
+    TriangleR(B)
+    Triangle_R_Iso(B)
+    Triangle_Equi(B)
+    Parallelogramme(B)
+
+def carré(B):
+    x=[]
+    y=[]
+    E=[]
+    o=0
+    #combinations de la liste B car un carré n'a que 4 cotés donc 4 points
+    E=list(combinations(B,4))
+    print(E)
+    for i in range(len(E)):
+        #calcul des distances a l'aide des formules vectorielles
+        AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+        AD=sqrt((E[i][3][0]-E[i][0][0])**2+(E[i][3][1]-E[i][0][1])**2)
+        BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+        CD=sqrt((E[i][3][0]-E[i][2][0])**2+(E[i][3][1]-E[i][2][1])**2)
+        AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+        BD=sqrt((E[i][3][0]-E[i][1][0])**2+(E[i][3][1]-E[i][1][1])**2)
+        #coordonnée du vecteurs AB et BC
+        xAB=(E[i][1][0]-E[i][0][0])
+        yAB=(E[i][1][1]-E[i][0][1])
+        xBC=(E[i][2][0]-E[i][1][0])
+        yBC=(E[i][2][1]-E[i][1][1])
+        #produit scalaire entre AB et BC afin s'ils sont perpendiculaire et qu'il ya un angle droit
+        k=((xAB)*(xBC))+((yAB)*(yBC))
+        if (AB==AD==BC==CD) and (k==0):
+            o=o+1
+            x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][3][0],E[i][0][0]]
+            y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][3][1],E[i][0][1]]
+            plt.plot(x,y)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("Tous les carrés")
+    print("En tout il y'a ",o,"carré")
+    plt.show()
+
+def Rectangle(B):
+        x=[]
+        y=[]
+        E=[]
+        m=0
+        E=list(combinations(B,4))
+        for i in range(len(E)):
+            AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+            AD=sqrt((E[i][3][0]-E[i][0][0])**2+(E[i][3][1]-E[i][0][1])**2)
+            BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+            CD=sqrt((E[i][3][0]-E[i][2][0])**2+(E[i][3][1]-E[i][2][1])**2)
+            xAB=(E[i][1][0]-E[i][0][0])
+            yAB=(E[i][1][1]-E[i][0][1])
+            xBC=(E[i][2][0]-E[i][1][0])
+            yBC=(E[i][2][1]-E[i][1][1])
+            #toujours le produit scalaire pour determiner s'il ya un angle droit
+            PABBC=((xAB)*(xBC))+((yAB)*(yBC))
+            if(AB==CD) and (AD==BC) and (AB!=BC) and (PABBC==0):
+                m=m+1
+                x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][3][0],E[i][0][0]]
+                y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][3][1],E[i][0][1]]
+                plt.plot(x,y)
+                plt.xlabel("X")
+                plt.ylabel("Y")
+                plt.title("Tous les rectangles")
+        print("Il ya",m,"rectangle")
+        plt.show()
+
+def TriangleR(B):
+    x=[]
+    y=[]
+    E=[]
+    v=0
+    #combinaisons de 3 car un triangle rectangke n'a que 3 cotés et donc trois points relié entre eux
+    E=list(combinations(B,3))
+    for i in range(len(E)):
+        AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+        AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+        BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+        h=[AB,AC,BC]
+        h.sort()
+        # Ajout de sécurités pour vérifier que les points ne sont pas alignés
+
+        # Calcul de l'hypoténuse noté h
+        if  h[2]**2==h[0]**2+h[1]**2 :
+            v=v+1
+            x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][0][0]]
+            y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][0][1]]
+            plt.plot(x,y)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("Tous les triangles rectangles")
+    print("Il ya ",v,"triangle rectangle")
+    plt.show()
+def Triangle_R_Iso(B):
+    x=[]
+    y=[]
+    E=[]
+    v=0
+    E=list(combinations(B,3))
+    for i in range(len(E)):
+        AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+        AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+        BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+        h=[AB,AC,BC]
+        h.sort()
+        if  h[2]**2==h[0]**2+h[1]**2 and h[0]==h[1] and h[2]!=h[0]:
+            v=v+1
+            x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][0][0]]
+            y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][0][1]]
+            plt.plot(x,y)
+    print("Il ya",v,"triangle rectangle isocele")
+    plt.show()
+def Triangle_Iso(B):
+    x=[]
+    y=[]
+    E=[]
+    v=0
+    E=list(combinations(B,3))
+    for i in range(len(E)):
+        AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+        AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+        BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+        if (AB==AC and BC!=AB) or (AB==BC and AC!=BC) or (AC==BC) :
+            v=v+1
+            plt.plot(x,y)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("Tous les triangles isocele")
+    print("il y'a",v,"Triangle Isocele")
+    plt.show()
+def Triangle_Equi(B):
+    x=[]
+    y=[]
+    E=[]
+    v=0
+    E=list(combinations(B,3))
+    for i in range(len(E)):
+        AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+        AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+        BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+        if  (AB==BC==AC):
+            x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][0][0]]
+            y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][0][1]]
+            plt.plot(x,y)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("Tous les triangles equilatéral")
+    print("Il ya" ,v," triangle equilatéral")
+    plt.show()
+
+def Parallelogramme(B):
+    A=[]
+    D=[]
+    x=[]
+    y=[]
+    S=[]
+    A=list(combinations(B, 4))
+    for i in range(len(A)):
+        if (((A[i][0][0]),(A[i][0][1])) != ((A[i][1][0]), (A[i][1][1])) != ((A[i][2][0]), (A[i][2][1])) != (
+        (A[i][3][0]), (A[i][3][1])) != ((A[i][0][0]), (A[i][0][1]))):
+            AB = sqrt(((A[i][1][0] - A[i][0][0]) ** 2) + ((A[i][1][1] - A[i][0][1]) ** 2))
+            AD = sqrt(((A[i][3][0] - A[i][0][0]) ** 2) + ((A[i][3][1] - A[i][0][1]) ** 2))
+            BC = sqrt(((A[i][2][0] - A[i][1][0]) ** 2) + ((A[i][2][1] - A[i][1][1]) ** 2))
+            CD = sqrt(((A[i][3][0] - A[i][2][0]) ** 2) + ((A[i][3][1] - A[i][2][1]) ** 2))
+            if AB == CD and BC == AD:
+                S.append("c'est un parallelogramme ")
+                x = [A[i][0][0], A[i][1][0], A[i][2][0], A[i][3][0], A[i][0][0]]
+                y = [A[i][0][1], A[i][1][1], A[i][2][1], A[i][3][1], A[i][0][1]]
+                plt.plot(x, y)
+                plt.scatter(x, y)
+                plt.xlabel("X")
+                plt.ylabel("Y")
+                plt.title("parallellogramme")
+    n=0
+    for i in range (len(S)):
+        n=n+1
+    print("il y'a",(n),"parallélogrammes")
+    plt.show()
+
+def losange(B):
+    x=[]
+    y=[]
+    E=[]
+    v=1
+    E=list(combinations(B,4))
+    for i in range(len(E)):
+        if (((E[i][0][0]),(E[i][0][1])) != ((E[i][1][0]), (E[i][1][1])) != ((E[i][2][0]), (E[i][2][1])) != (
+        (E[i][3][0]), (E[i][3][1])) != ((E[i][0][0]), (A[i][0][1]))):
+        #Les distances
+            AB=sqrt((E[i][1][0]-E[i][0][0])**2+(E[i][1][1]-E[i][0][1])**2)
+            AD=sqrt((E[i][3][0]-E[i][0][0])**2+(E[i][3][1]-E[i][0][1])**2)
+            BC=sqrt((E[i][2][0]-E[i][1][0])**2+(E[i][2][1]-E[i][1][1])**2)
+            CD=sqrt((E[i][3][0]-E[i][2][0])**2+(E[i][3][1]-E[i][2][1])**2)
+            AC=sqrt((E[i][2][0]-E[i][0][0])**2+(E[i][2][1]-E[i][0][1])**2)
+            BD=sqrt((E[i][3][0]-E[i][1][0])**2+(E[i][3][1]-E[i][1][1])**2)
+            #produit scalaire entre AB et BC afin s'ils sont perpendiculaire et qu'il ya un angle droit
+            xAB=(E[i][1][0]-E[i][0][0])
+            yAB=(E[i][1][1]-E[i][0][1])
+            xBC=(E[i][2][0]-E[i][1][0])
+            yBC=(E[i][2][1]-E[i][1][1])
+            k=((xAB)*(xBC))+((yAB)*(yBC))
+            #Le point E determine l'intersection des diagonales AC et BD
+            #La distance BE est égale a la dsitance de la diagonale BD divisé par deux
+            BE=(BD)/2
+            #La distances AE est égale a la distance de la diagonale AC divisé par deux
+            AE=(AC)/2
+            #formule pour reconnaitre un parrallelograme
+            for i in range (len(E)):
+                if (((E[i][0][0]),(E[i][0][1])) != ((E[i][1][0]), (E[i][1][1])) != ((E[i][2][0]), (E[i][2][1])) != (
+        (E[i][3][0]), (E[i][3][1])) != ((E[i][0][0]), (E[i][0][1]))):
+                 if (AC*2+BD*2)==2(AB*2+BC*2) and ((BE)!=(AE)) and (k!=0):
+                    v=v+1
+                    x=[E[i][0][0],E[i][1][0],E[i][2][0],E[i][3][0],E[i][0][0]]
+                    y=[E[i][0][1],E[i][1][1],E[i][2][1],E[i][3][1],E[i][0][1]]
+                    plt.plot(x,y)
+                    plt.xlabel("X")
+                    plt.ylabel("Y")
+                    plt.title("Tous les losanges")
+        print("Il y'a",v," losange")
+        plt.show()
